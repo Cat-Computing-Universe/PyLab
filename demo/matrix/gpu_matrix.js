947dbceda451ff03b0c3957fd1922a519de58d6e
brian-lineup <bboczeng@gmail.com>
2022-06-20 14:55:05 -0700

    Update gpu_matrix.js

--- demo/matrix/gpu_matrix.js
+++ demo/matrix/gpu_matrix.js
@@ -13,8 +13,23 @@ export class GPUMatrixMultiplier {
         this.gpu = new GPU();
         console.log(this.gpu);
         // add numpy matrix conversion here
-        this.matrixA = a;
-        this.matrixB = b;
+        // converting for Matrix A, B
+        for (let y = 0; y < matASizeI; y++) {
+            this.matrixA.push([])
+            for (let x = 0; x < matASizeJ; x++) {
+              const value = parseInt((Math.random() * 10).toString())
+              this.matrixA[y].push(value)
+            }
+        }
+
+        for (let y = 0; y < matBSizeI; y++) {
+            this.matrixB.push([])
+            for (let x = 0; x < matBSizeJ; x++) {
+              const value = parseInt((Math.random() * 10).toString())
+              this.matrixB[y].push(value)
+            }
+        }
+
         this.matrixResult = [];
     }
 
