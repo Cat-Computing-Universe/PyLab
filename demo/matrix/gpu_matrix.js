aaefb5aafcbd2205947e1a62278bfad338c2ada0
brian-lineup <bboczeng@gmail.com>
2022-06-20 15:59:43 -0700

    update GPU example

--- demo/matrix/gpu_matrix.js
+++ demo/matrix/gpu_matrix.js
@@ -1,33 +1,21 @@
 function gpumatmul(a, b) {
     const gpu = new GPU();
-    console.log(a);
-    console.log(b);
-    // add numpy matrix conversion here
-    // converting for Matrix A, B
-    for (let y = 0; y < matASizeI; y++) {
-        this.matrixA.push([])
-        for (let x = 0; x < matASizeJ; x++) {
-            const value = parseInt((Math.random() * 10).toString())
-            this.matrixA[y].push(value)
-        }
-    }
 
-    for (let y = 0; y < matBSizeI; y++) {
-        this.matrixB.push([])
-        for (let x = 0; x < matBSizeJ; x++) {
-            const value = parseInt((Math.random() * 10).toString())
-            this.matrixB[y].push(value)
-        }
-    }
-    this.matrixResult = [];
-    const multiplyMatrix = gpu.createKernel(function (a, b) {
+    matSizeI = a.length;
+    matSizeJ = a[0].length;
+    matSizeK = b[0].length;
+
+    // add error handling
+
+    const multiplyMatrix = gpu.createKernel(function (a, b, size) {
         let sum = 0;
-        for (let i = 0; i < this.matSizeJ; i++) {
+        for (let i = 0; i < size; i++) {
             sum += a[this.thread.y][i] * b[i][this.thread.x];
         }
         return sum;
-    }).setOutput([this.matSizeI, this.matSizeK])
+    }).setOutput([matSizeI, matSizeK])
 
-    return multiplyMatrix(this.matrixA, this.matrixB);
-
+    const result = multiplyMatrix(a, b, matSizeJ);
+    return result
+    // todo: convert to arrary?
 }
