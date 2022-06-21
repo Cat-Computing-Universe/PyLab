3169cc112c67beab4a85d4f36cf29c4f61217848
brian-lineup <bboczeng@gmail.com>
2022-06-20 18:37:07 -0700

    add gpu util

--- computing_core/gpu_matmul.py
+++
@@ -1,2 +0,0 @@
-# This file defines the necessary decorated functions for 2D Matrix GPU multiplication
-
---
+++ computing_core/gpu_util.py
@@ -0,0 +1,9 @@
+# This file defines the necessary decorated functions for 2D Matrix GPU multiplication
+import numpy as np
+from js import gpumatmul
+from pyodide import to_js
+
+def gmm(a, b, to_numpy=False):
+    result = gpumatmul(to_js(a), to_js(b))
+    if to_numpy:
+        np.array(result.to_py())
